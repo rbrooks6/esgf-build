7758487ea82c8be7398a15ed06cd542da0b0479f
William Hill <hill119@llnl.gov>
2017-06-27 16:17:44 -0700

    removed hard coded subpaths

--- esgfupload.sh
+++ esgfupload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-	rsync -arWvu $HOME/esgf-work/dist-repos/prod/ -e ssh --delete esgf@distrib-coffee.ipsl.jussieu.fr:/home/esgf/esgf/ 2>&1 |tee esgfupload.log
+	rsync -arWvu dist-repos/prod/ -e ssh --delete esgf@distrib-coffee.ipsl.jussieu.fr:/home/esgf/esgf/ 2>&1 |tee esgfupload.log
 else
-	rsync -arWvun $HOME/esgf-work/dist-repos/prod/ -e ssh --delete esgf@distrib-coffee.ipsl.jussieu.fr:/home/esgf/esgf/ 2>&1 |tee esgfupload.log
+	rsync -arWvunO dist-repos/prod/ -e ssh --delete esgf@distrib-coffee.ipsl.jussieu.fr:/home/esgf/esgf/ 2>&1 |tee esgfupload.log
 fi
