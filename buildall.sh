4337aac19a92cf78356be8b3dc5c444fc1a31f87
rbrooks6 <rbrooks6@hawk.iit.edu>
2017-07-20 16:30:43 -0700

    buildall.sh translated and build_all function commented and framework set

--- buildall.sh
+++ buildall.sh
@@ -9,36 +9,49 @@ PYTHONDIR="$(dirname $(which python))"
 echo "PYTHONDIR: ${PYTHONDIR}"
 LOGDIR=$PWD/buildlogs
 
+## v:export variables are available to other programs
+##???????? A check for if there is a path to java and python and if not, then creates a path?
 export JAVA_HOME=$JAVADIR
-if ! echo $PATH|grep "$JAVADIR" >/dev/null; then 
+if ! echo $PATH|grep "$JAVADIR" >/dev/null; then
 	echo "Will prepend path with custom java";
 	export PATH=$JAVADIR:$PATH;
 fi
-if ! echo $PATH|grep "$PYTHONDIR" >/dev/null; then 
+##grep finds and searches for everything with pythondir in it and prints it to null (nowhere)
+if ! echo $PATH|grep "$PYTHONDIR" >/dev/null; then
 	echo "Will prepend path with custom python";
 	export PATH=$PYTHONDIR:$PATH;
 fi
 
 #If no command line arguments; build all repos.  Otherwise build only repos passed as command line arguments
+## v: $# prints the number of arguments passed to the shell script -eq checks for equality
 if [ $# -eq 0 ]; then
 	#Uses mapfile CLI tool that's part of Bash version 4
+	##creates an array called fulllist and reads input from repo_list into it after finding repo_list in the current directory
 	mapfile -t fulllist < "$(dirname -- "$0")/repo_list.txt"
 	echo "fulllist: ${fulllist[*]}"
 	echo
 else
+	## if there are command line arguments, then those are taken and used in fulllist
 	fulllist=("${@:1}")
 	echo "fulllist: ${fulllist[*]}"
 	echo
 fi
 
 
+#################################################
+##iterates over the array and logs <pwd>/buildlogs/<repositoryname>
 for i in "${fulllist[@]}"; do
+	##clean pull build logs are created (ANT)
+	###opens 3 files
 	echo -n >$LOGDIR/$i-clean.log
 	echo -n >$LOGDIR/$i-pull.log
 	echo -n >$LOGDIR/$i-build.log
 	echo $i;
 	cd $i;
+	#if the esgf-getcert is buing built then
 	if [ "$i" = "esgf-getcert" ]; then
+		##tee: redirects output to multile files , input -> standard output and any files given as arguments
+		###will be done in a subprocess
 		$ANT clean 2>&1|tee $LOGDIR/$i-clean.log;
 		$ANT dist 2>&1|tee $LOGDIR/$i-build.log;
 		cd ..
@@ -51,5 +64,7 @@ for i in "${fulllist[@]}"; do
 	cd ..
 done
 
+##recursively searches for build in all repos
+##???????
 #Logs out the build result of all of the repos
 grep -R "BUILD" buildlogs/esg*-*-build.log
--- esgf_build.py
+++ esgf_build.py
@@ -1,14 +1,17 @@
-#!usr/bin/env python2
+'''Modules needed mostly to access terminal commands'''
 import subprocess
 import shlex
 import os
 from git import Repo
 import repo_info
+import tarfile
+from distutils.spawn import find_executable
 
 #repo_info.all_repo_urls
 #repo_info.repo_list
 
 def update_all(active_branch):
+    '''Checks each repo in the repo_list for the most updated branch '''
     #taglist will keep track of different versions
     fileobject = open("taglist.txt", "w")
     for repo in repo_info.repo_list:
@@ -27,9 +30,38 @@ def update_all(active_branch):
     fileobject.close()
 
 def build_all(build_list):
-    pass
+    '''Takes a list of repositories to build, and uses ant to build them '''
+    #use subprocess for ANT
+    #locate the paths for ANT, java, and python
+    ant_path = find_executable('ant')
+    java_path = find_executable('java')
+    python_path = find_executable('python')
+
+    log_directory = os.getcwd() + "/buildlogs"
+    for repo in build_list:
+        #creates clean pull and build logs for  each repo
+        clean_log = log_directory + "/" + repo + "-clean.log"
+        with open("clean_log.txt", "w") as f1:
+            pass
+        pull_log = log_directory + "/" + repo + "-pull.log"
+        with open("pull_log", "w") as f2:
+            f2.write("test")
+            print f2
+        build_log = log_directory + "/" + repo + "-build.log"
+        with open("build_log", "w") as f3:
+            pass
+
+        #TODO: call ANT esgf-getcert clean and build seperatley
+
+
+    #call ANT w/ subprocess
+    #ANT clean
+    #ANT pull
+    #ANT build
+    #ANT publishlocal
 
 def create_esgf_tarballs():
+    #import tarfile
     pass
 
 def create_local_mirror_directory(active_branch):
@@ -41,7 +73,9 @@ def update_esg_node(active_branch):
 def esgf_upload():
     pass
 
+
 def main():
+    '''User prompted for build specifications '''
     #Use a raw_input statement to ask the user if they want to update devel or master
     #The user's answer will set the active_branch variable; must either be devel or master
     while True:
@@ -49,8 +83,8 @@ def main():
 
         #Run the update_all(active_branch) function, passing in active_branch as an argument
         if active_branch.lower() not in ["devel", "master"]:
-             print "Please choose either master or devel."
-             continue
+            print "Please choose either master or devel."
+            continue
 
         else:
             break
@@ -64,7 +98,6 @@ def main():
     #Use a raw_input statement to ask the user to set the script_major_version, script_release, and
     #script_version
 
-
     #execute the create_esgf_tarballs() function
 
     #execute the create_local_mirror_directory(active_branch), passing in active_branch as an argument
@@ -72,7 +105,6 @@ def main():
     #execute update_esg_node(active_branch), passing in active_branch as an argument
 
     #execute esgf_upload()
-    pass
 
 if __name__ == '__main__':
     main()
