02468032dfeaee9cb3c6bc10d065c46980447efc
rbrooks6 <rbrooks6@hawk.iit.edu>
2017-07-21 16:55:14 -0700

    Added user prompt comments and primitive subprocess for ant

--- esgf_build.py
+++ esgf_build.py
@@ -1,3 +1,4 @@
+#!usr/bin/env python
 '''Modules needed mostly to access terminal commands'''
 import subprocess
 import shlex
@@ -12,7 +13,7 @@ from distutils.spawn import find_executable
 
 def update_all(active_branch):
     '''Checks each repo in the repo_list for the most updated branch '''
-    #taglist will keep track of different versions
+    taglist will keep track of different versions
     fileobject = open("taglist.txt", "w")
     for repo in repo_info.repo_list:
         os.chdir(repo)
@@ -29,6 +30,10 @@ def update_all(active_branch):
         os.chdir("..")
     fileobject.close()
 
+    ##TODO: For testing
+    print "Made it to update_all"
+    print "active_branch: ", active_branch
+
 def build_all(build_list):
     '''Takes a list of repositories to build, and uses ant to build them '''
     #use subprocess for ANT
@@ -40,25 +45,18 @@ def build_all(build_list):
     log_directory = os.getcwd() + "/buildlogs"
     for repo in build_list:
         #creates clean pull and build logs for  each repo
+        ###TODO: log ant output to clean, pull, and build logs for appropriate repo
         clean_log = log_directory + "/" + repo + "-clean.log"
         with open("clean_log.txt", "w") as f1:
-            pass
+            subprocess.call('ant allclean')
         pull_log = log_directory + "/" + repo + "-pull.log"
-        with open("pull_log", "w") as f2:
-            f2.write("test")
-            print f2
+        with open("pull_log.txt", "w") as f2:
+            subprocess.call("ant pull")
         build_log = log_directory + "/" + repo + "-build.log"
-        with open("build_log", "w") as f3:
-            pass
+        with open("build_log.txt", "w") as f3:
+            subprocess.call("ant make_dist")
 
         #TODO: call ANT esgf-getcert clean and build seperatley
-
-
-    #call ANT w/ subprocess
-    #ANT clean
-    #ANT pull
-    #ANT build
-    #ANT publishlocal
 
 def create_esgf_tarballs():
     #import tarfile
@@ -92,11 +90,25 @@ def main():
 
     #Use a raw_input statement to ask which repos should be built;
     #this will set the build_list variable; If the user enters nothing, assume all repos will be built
+    while True:
+        build_list = raw_input("Which repositories will be built? (Hit [Enter] for all)")
 
-    #Execute build_all(build_list), passing in the build_list variable as an argument
+        if not build_list:
+            all_repo_q =raw_input("Do you want to build all repositories? (Y or YES)")
+            if all_repo_q.lower() not in ["yes", "y"]:
+                print "Not a valid response."
+                continue
+
+            else:
+                build_list = repo_info.repo_list
+                break
+    build_all(build_list)
 
     #Use a raw_input statement to ask the user to set the script_major_version, script_release, and
     #script_version
+    script_major_version = raw_input("Please set the script_major_version: ")
+    script_release = raw_input("Please set the script_release: ")
+    script_version = ("Please set the script version: ")
 
     #execute the create_esgf_tarballs() function
 
