6bcc7086a692d8a54117b88f7bb0a223fc90e04c
rbrooks6 <rbrooks6@hawk.iit.edu>
2017-07-26 11:46:38 -0700

    Wrote purge and clone in python, wrote build all

---
+++ purge_and_clone_fresh_repos.py
@@ -0,0 +1,34 @@
+#!usr/bin/env python
+import shutil
+import repo_info
+from git import Repo
+import os
+import re
+
+#TODO: remove esgf installer from repo list when not on personal computer
+
+#Search for and remove appropriate repos
+for repo in repo_info.repo_list:
+    if repo == 'esgf-installer':
+        continue
+    try:
+        shutil.rmtree('/Users/brooks47/development/' + repo)
+        print(repo + " removed succesfully.")
+    except:
+        print(repo + " does not exist on this system.")
+        print( repo + " skipped.")
+
+
+for repo_url in repo_info.all_repo_urls:
+    #TODO: to make universal, allow user to enter the directory path to clone to
+
+    ###In the future, if a module with more than 2 "-"s is created, this will
+    ###need to be revised
+    #strips.git from the string to make using reg-ex easier
+    strip_repo = repo_url.replace(".git", "")
+    directory_name = re.search('esg\w*-\w+\W*\w+', strip_repo).group()
+    if directory_name == 'esgf-installer':
+        continue
+    print("Currently working on directory: " + directory_name)
+    Repo.clone_from(repo_url, '/Users/brooks47/development/' + directory_name)
+    print(directory_name + " succesfully cloned.")
